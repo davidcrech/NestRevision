775114b1607f3f22d4731aa2ab4855c5d7de8163
davidcrech <davidcrech220@gmail.com>
2025-07-04 17:42:42 -0300

    feat: add import to frame schema in frame module

--- src/frames/frames.module.ts
+++ src/frames/frames.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { FramesService } from './frames.service';
 import { FramesController } from './frames.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Frame, FrameSchema } from './schemas/frames.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Frame.name,
+        schema: FrameSchema,
+      },
+    ]),
+  ],
   controllers: [FramesController],
   providers: [FramesService],
 })
